5938c69efd9f8cf62531c896d3dffc38aa3b6914
OxiFoxy10 <oximuzyka@gmail.com>
2022-03-28 17:32:33 +0300

    connection typescript error fixed

--- src/server/database/connection.ts
+++ src/server/database/connection.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connect = {
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
